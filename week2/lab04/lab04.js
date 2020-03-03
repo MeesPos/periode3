4c0746e5d38f25cbb7ba8e01479ba290e51d3172
MeesPos <meespostma_@hotmail.com>
2020-03-03 14:47:30 +0100

    Lab 04 af

--- week2/lab04/lab04.js
+++ week2/lab04/lab04.js
@@ -1,16 +1,19 @@
 // Pas het script aan volgens de richtlijnen
-var palet = new Array('wit','blauw','groen','rood','zwart');
+let palet = new Array('wit','blauw','groen','rood','zwart');
 document.write('<br />palet is nu: ' + palet);
 
-var nieuwekleur = prompt('Typ een nieuwe kleur in');
+let nieuwekleur = prompt('Typ een nieuwe kleur in');
 
 // Stop de nieuwekleur ACHTERAAN in de palet array
+palet.push(nieuwekleur);
 document.write('<br />palet is nu: ' + palet);
 
 // Stop de nieuwekleur VOORAAN in de palet array
-var nieuwekleur = prompt( 'Typ een nieuwe kleur in:');
+palet.unshift(nieuwekleur);
+nieuwekleur = prompt( 'Typ een nieuwe kleur in:');
 
 document.write('<br />palet is nu: ' + palet);
 // Sorteer de array
 
+palet.sort();
 document.write('<br />palet is nu: ' + palet);
